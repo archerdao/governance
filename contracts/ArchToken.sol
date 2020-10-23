26c4d50ca767529c42c465d930372fdaec8c463e
Pyython <vogonpoetry06@gmail.com>
2020-10-23 16:36:48 +0700

    added EIP-3009

--- contracts/ArchToken.sol
+++ contracts/ArchToken.sol
@@ -44,13 +44,26 @@ contract ArchToken {
     mapping (address => uint256) internal balances;
 
     /// @notice The EIP-712 typehash for the contract's domain
-    bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+    /// keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
+    bytes32 public constant DOMAIN_TYPEHASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
+    
+    /// @notice The EIP-712 version hash
+    /// keccak256("1");
+    bytes32 public constant VERSION_HASH = 0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6;
 
     /// @notice The EIP-712 typehash for the permit struct used by the contract
-    bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    /// keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+
+    /// @notice The EIP-3009 typehash for transferWithAuthorization
+    /// keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)");
+    bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH = 0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
 
     /// @notice A record of states for signing / validating signatures
     mapping (address => uint) public nonces;
+
+    /// @dev authorizer address > nonce > state (true = used / false = unused)
+    mapping (address => mapping (bytes32 => bool)) public authorizationState;
 
     /// @notice An event that's emitted when the mintCap is changed
     event MintCapChanged(uint16 indexed oldMintCap, uint16 indexed newMintCap);
@@ -72,6 +85,9 @@ contract ArchToken {
 
     /// @notice The standard EIP-20 approval event
     event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    /// @notice An event that's emitted whenever an authorized transfer occurs
+    event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
 
     /**
      * @notice Construct a new Arch token
@@ -187,34 +203,28 @@ contract ArchToken {
      * @param amount The number of tokens that are approved (2^256-1 means infinite)
      * @return Whether or not the approval succeeded
      */
-    function approve(address spender, uint amount) external returns (bool) {
-        allowances[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(msg.sender, spender, amount);
         return true;
     }
 
     /**
-     * @notice Triggers an approval from owner to spends
+     * @notice Triggers an approval from owner to spender
      * @param owner The address to approve from
      * @param spender The address to be approved
-     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     * @param value The number of tokens that are approved (2^256-1 means infinite)
      * @param deadline The time at which to expire the signature
      * @param v The recovery byte of the signature
      * @param r Half of the ECDSA signature pair
      * @param s Half of the ECDSA signature pair
      */
-    function permit(address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
-        bytes32 domainSeparator = keccak256(abi.encode(DOMAIN_TYPEHASH, keccak256(bytes(name)), _getChainId(), address(this)));
-        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, amount, nonces[owner]++, deadline));
-        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
-        address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "Arch::permit: invalid signature");
-        require(signatory == owner, "Arch::permit: unauthorized");
-        require(block.timestamp <= deadline, "Arch::permit: signature expired");
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(deadline >= block.timestamp, "Arch::permit: signature expired");
 
-        allowances[owner][spender] = amount;
+        bytes32 encodeData = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
+        _validateSignedData(owner, encodeData, v, r, s);
 
-        emit Approval(owner, spender, amount);
+        _approve(owner, spender, value);
     }
 
     /**
@@ -259,6 +269,98 @@ contract ArchToken {
         return true;
     }
 
+    /**
+     * @notice Transfer tokens with a signed authorization
+     * @param from Payer's address (Authorizer)
+     * @param to Payee's address
+     * @param value Amount to be transferred
+     * @param validAfter The time after which this is valid (unix time)
+     * @param validBefore The time before which this is valid (unix time)
+     * @param nonce Unique nonce
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external
+    {
+        require(block.timestamp > validAfter, "Arch::transferWithAuth: auth not yet valid");
+        require(block.timestamp < validBefore, "Arch::transferWithAuth: auth expired");
+        require(!authorizationState[from][nonce],  "Arch::transferWithAuth: auth already used");
+
+        bytes32 encodeData = keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce));
+        _validateSignedData(from, encodeData, v, r, s);
+
+        authorizationState[from][nonce] = true;
+        emit AuthorizationUsed(from, nonce);
+
+        _transferTokens(from, to, value);
+    }
+
+    /**
+     * @notice EIP-712 Domain separator
+     * @return Separator
+     */
+    function getDomainSeparator() public view returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                VERSION_HASH,
+                _getChainId(),
+                address(this)
+            )
+        );
+    }
+
+    /**
+     * @notice Recovers address from signed data and validates the signature
+     * @param signer Address that signed the data
+     * @param encodeData Data signed by the address
+     * @param v The recovery byte of the signature
+     * @param r Half of the ECDSA signature pair
+     * @param s Half of the ECDSA signature pair
+     */
+    function _validateSignedData(address signer, bytes32 encodeData, uint8 v, bytes32 r, bytes32 s) internal view {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                getDomainSeparator(),
+                encodeData
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        // Explicitly disallow authorizations for address(0) as ecrecover returns address(0) on malformed messages
+        require(recoveredAddress != address(0) && recoveredAddress == signer, "Arch::validateSig: invalid signature");
+    }
+
+    /**
+     * @notice Approval implementation
+     * @param owner The address of the account which owns tokens
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     */
+    function _approve(address owner, address spender, uint256 amount) internal {
+        allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /**
+     * @notice Transfer implementation
+     * @param src The address of the account which owns tokens
+     * @param dst The address of the account which is receiving tokens
+     * @param amount The number of tokens that are being transferred
+     */
     function _transferTokens(address src, address dst, uint256 amount) internal {
         require(src != address(0), "Arch::_transferTokens: cannot transfer from the zero address");
         require(dst != address(0), "Arch::_transferTokens: cannot transfer to the zero address");
@@ -268,6 +370,10 @@ contract ArchToken {
         emit Transfer(src, dst, amount);
     }
 
+    /**
+     * @notice Current id of the chain where this contract is deployed
+     * @return Chain id
+     */
     function _getChainId() internal pure returns (uint) {
         uint256 chainId;
         assembly { chainId := chainid() }
