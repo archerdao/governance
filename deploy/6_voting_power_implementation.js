910349da2b3f3b1564bf8696206f99ddcaa5847c
Pyython <vogonpoetry06@gmail.com>
2021-02-10 18:21:34 +0700

    deployment updates

--- deploy/6_voting_power_implementation.js
+++ deploy/6_voting_power_implementation.js
@@ -1,7 +1,6 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
-  const { deployer, admin } = await getNamedAccounts();
-  const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+  const { deployer } = await getNamedAccounts();
 
   log(`6) Voting Power Implementation`)
   // Deploy VotingPower implementation contract
@@ -14,15 +13,6 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   if (deployResult.newlyDeployed) {
     log(`- ${deployResult.contractName} deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`);
-    const deployerSigner = await ethers.getSigner(deployer)
-    const VotingPowerImpDeployment = await deployments.get("VotingPower")
-    const VotingPowerPrismDeployment = await deployments.get("VotingPowerPrism")
-    const VotingPower = new ethers.Contract(VotingPowerPrismDeployment.address, VotingPowerImpDeployment.abi, deployerSigner)
-    const vpOwner = await VotingPower.owner()
-    if (vpOwner == ZERO_ADDRESS) {
-      await VotingPower.changeOwner(admin)
-      log(`- Changed Voting Power owner to ${admin}`)
-    }
   } else {
     log(`- Deployment skipped, using previous deployment at: ${deployResult.address}`)
   }
--- scripts/configureRewardsManager.js
+++ scripts/configureRewardsManager.js
@@ -16,29 +16,33 @@ async function configureRewardsManager() {
         const treasury = await ethers.provider.getSigner(DAO_TREASURY_ADDRESS)
 
         const ArchTokenDeployment = await deployments.get("ArchToken")
-        const RewardsManager = await deployments.get("RewardsManager")
-        const LockManager = await deployments.get("LockManager")
+        const RewardsManagerDeployment = await deployments.get("RewardsManager")
+        const LockManagerDeployment = await deployments.get("LockManager")
         const Vault = await deployments.get("Vault")
         const TokenRegistry = await deployments.get("TokenRegistry")
         const VotingPowerImpDeployment = await deployments.get("VotingPower")
-        const ArchToken = new ethers.Contract(ArchTokenDeployment.address, ArchTokenDeployment.abi, deployer)
-        const VotingPowerImp = new ethers.Contract(VotingPowerImpDeployment.address, VotingPowerImpDeployment.abi, admin)
         const VotingPowerPrismDeployment = await deployments.get("VotingPowerPrism")
+
+        const ArchToken = new ethers.Contract(ArchTokenDeployment.address, ArchTokenDeployment.abi, admin)
+        const LockManager = new ethers.Contract(LockManagerDeployment.address, LockManagerDeployment.abi, admin)
+        const VotingPowerImp = new ethers.Contract(VotingPowerImpDeployment.address, VotingPowerImpDeployment.abi, admin)
         const VotingPowerPrism = new ethers.Contract(VotingPowerPrismDeployment.address, VotingPowerPrismDeployment.abi, admin)
         const VotingPower = new ethers.Contract(VotingPowerPrismDeployment.address, VotingPowerImpDeployment.abi, admin)
+        const RewardsManager = new ethers.Contract(RewardsManagerDeployment.address, RewardsManagerDeployment.abi, admin)
 
         await deployer.sendTransaction({ to: DAO_TREASURY_ADDRESS, value: ethers.utils.parseEther("0.05")})
         await ArchToken.connect(treasury).transfer(ADMIN_ADDRESS, INITIAL_ARCH_REWARDS_BALANCE);
         await ethers.provider.send('hardhat_stopImpersonatingAccount', [DAO_TREASURY_ADDRESS]);
         await deployer.sendTransaction({ to: ADMIN_ADDRESS, value: ethers.utils.parseEther("0.05")})
-        await VotingPowerPrism.connect(admin).setPendingProxyImplementation(VotingPowerImpDeployment.address);
-        await VotingPowerImp.connect(admin).become(VotingPowerPrism.address);
-        await VotingPower.connect(admin).setLockManager(LockManager.address)
-        await VotingPower.connect(admin).setTokenRegistry(TokenRegistry.address)
-        await LockManager.connect(admin).grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("LOCKER_ROLE")), RewardsManager.address)
-        await LockManager.connect(admin).grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("LOCKER_ROLE")), Vault.address)
-        await ArchToken.connect(admin).approve(RewardsManager.address, INITIAL_ARCH_REWARDS_BALANCE)
-        await RewardsManager.connect(admin).addRewardsBalance(INITIAL_ARCH_REWARDS_BALANCE)
+        await VotingPowerPrism.setPendingProxyImplementation(VotingPowerImpDeployment.address);
+        await VotingPowerImp.become(VotingPowerPrism.address);
+        await VotingPower.changeOwner(ADMIN_ADDRESS)
+        await VotingPower.setLockManager(LockManager.address)
+        await VotingPower.setTokenRegistry(TokenRegistry.address)
+        await LockManager.grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("LOCKER_ROLE")), RewardsManager.address)
+        await LockManager.grantRole(ethers.utils.keccak256(ethers.utils.toUtf8Bytes("LOCKER_ROLE")), Vault.address)
+        await ArchToken.approve(RewardsManager.address, INITIAL_ARCH_REWARDS_BALANCE)
+        await RewardsManager.addRewardsBalance(INITIAL_ARCH_REWARDS_BALANCE)
     } else {
         log("Must configure manually using multi-sig")
     }
